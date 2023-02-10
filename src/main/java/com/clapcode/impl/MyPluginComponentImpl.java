0f682b1dd5815ec3f9df0ac0bf87520e162a7f9b
Arnab bhowmik <13arnabbhowmik@gmail.com>
2023-02-10 22:44:16 +0530

    first commit

---
+++ src/main/java/com/clapcode/impl/MyPluginComponentImpl.java
@@ -0,0 +1,25 @@
+package com.clapcode.impl;
+
+import com.atlassian.sal.api.ApplicationProperties;
+import com.clapcode.api.MyPluginComponent;
+
+
+public class MyPluginComponentImpl implements MyPluginComponent
+{
+        private final ApplicationProperties applicationProperties;
+
+        public MyPluginComponentImpl(final ApplicationProperties applicationProperties)
+    {
+        this.applicationProperties = applicationProperties;
+    }
+
+    public String getName()
+    {
+        if(null != applicationProperties)
+        {
+            return "myComponent:" + applicationProperties.getDisplayName();
+        }
+        
+        return "myComponent";
+    }
+}
